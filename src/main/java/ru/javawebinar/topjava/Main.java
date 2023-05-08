473c9a7ce30f9173ff6cf9be846c32ca5c9c3b23
Vasili Bandarenka <darken123@inbox.ru>
2023-05-08 12:39:36 +0300

    realize HW0

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -1,5 +1,10 @@
 package ru.javawebinar.topjava;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @see <a href="http://topjava.herokuapp.com">Demo application</a>
  * @see <a href="https://github.com/JavaOPs/topjava">Initial project</a>
@@ -7,5 +12,40 @@ package ru.javawebinar.topjava;
 public class Main {
     public static void main(String[] args) {
         System.out.format("Hello TopJava Enterprise!");
+        //TODO удалить тесты ниже
+        System.out.println();
+        Map<String, Integer> myMap = new HashMap<>();
+        myMap.put("first", 11);
+        myMap.put("second", 22);
+        myMap.put("third", 33);
+        myMap.put("fourth", 44);
+
+        for (Map.Entry<String, Integer> entry : myMap.entrySet()) {
+            System.out.println(entry);
+        }
+        System.out.println();
+        Integer newKey = myMap.merge("new key", 444, (v1, v2) -> v1 + v2);
+        Integer second = myMap.merge("second", 6, (v1, v2) -> v1 + v2);
+
+        System.out.println();
+        for (Map.Entry<String, Integer> entry : myMap.entrySet()) {
+            System.out.println(entry);
+        }
+//        Set<Map.Entry<String, Integer>> entrySet = myMap.entrySet();
+//        System.out.println("простой цикл");
+//        for (int i = 0; i <entrySet.size(); i++) {
+//            System.out.println(entrySet.toArray()[i]);
+//        }
+//        Set<String> strings = myMap.keySet();
+//        for (String key: strings) {
+//            System.out.println("Key " + myMap.get(key) + " value " + key);
+//        }
+//        Collection<Integer> values = myMap.values();
+//        for (Integer val: myMap.values()){
+//            System.out.println("value " + val);
+//        }
+    }
+    private static Integer apply(Integer v1, Integer v2) {
+        return v1 + v2 + 2;
     }
 }
---
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -0,0 +1,68 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.model.UserMealWithExcess;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class UserMealsUtil {
+    public static void main(String[] args) {
+        List<UserMeal> meals = Arrays.asList(
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
+        );
+
+        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        mealsTo.forEach(System.out::println);
+
+        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+    }
+
+    public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        Map<LocalDate, Integer> everydayCaloriesMap = new TreeMap<>();
+        for (UserMeal meal : meals) {
+            LocalDate date = meal.getDateTime().toLocalDate();
+            everydayCaloriesMap.merge(date, meal.getCalories(), Integer::sum);
+        }
+
+        List<UserMealWithExcess> userMealWithExcessList = new ArrayList<>();
+        for (UserMeal meal : meals) {
+            LocalDate date = meal.getDateTime().toLocalDate();
+            LocalTime time = meal.getDateTime().toLocalTime();
+
+            boolean excess = everydayCaloriesMap.get(date) > caloriesPerDay;
+            if (TimeUtil.isBetweenHalfOpen(time, startTime, endTime)) {
+                userMealWithExcessList.add(
+                        new UserMealWithExcess(
+                                meal.getDateTime(),
+                                meal.getDescription(),
+                                meal.getCalories(),
+                                excess));
+            }
+        }
+        return userMealWithExcessList;
+    }
+
+    public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+
+        Map<LocalDate, Integer> collect = meals.stream()
+                .collect(Collectors.groupingBy(meal -> meal.getDateTime().toLocalDate(), Collectors.summingInt(meal-> meal.getCalories())));
+
+        return meals.stream()
+                .filter(userMeal -> TimeUtil.isBetweenHalfOpen(userMeal.getDateTime().toLocalTime(), startTime, endTime))
+                .map(userMeal -> new UserMealWithExcess(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(),
+                        collect.get(userMeal.getDateTime().toLocalDate()) >caloriesPerDay))
+        .collect(Collectors.toList());
+    }
+}
